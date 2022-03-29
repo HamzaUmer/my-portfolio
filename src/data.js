987f9b6183fbd3bb8aa7b41b9c0c7518bf3e3e63
Mirza Hamza Umer <51873193+HamzaUmer@users.noreply.github.com>
2022-03-29 23:55:55 +0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,8 +2,12 @@ import Img1 from "./img/AvengersWebsite.png";
 import Img2 from "./img/UniversityWebsite.png";
 import Img3 from "./img/meetupApp.png";
 import Img4 from "./img/MERNapp.png";
-import Img5 from "./img/ReenWebsite.png";
-import Img6 from "./img/BunnyWebsite.png";
+import Img5 from "./img/eBookStore.png";
+import Img6 from "./img/ScrambleWords.png";
+import Img7 from "./img/ARMart.png";
+import Img8 from "./img/MusicPlayer.png";
+import Img9 from "./img/NextJS.png";
+
 export const products = [
     {
       id: 1,
@@ -28,11 +32,26 @@ export const products = [
     {
       id: 5,
       img: Img5,
-      link: "https://e-commerceappmern.herokuapp.com/",
+      link: "https://ebook-store-app.vercel.app/",
     },
     {
       id: 6,
       img: Img6,
-      link: "https://hamzaumer.github.io/Bunny-The-Developer-React-Website/",
+      link: "https://hamzaumer.github.io/Scrambled-Words-ES6/",
+    },
+    {
+      id: 7,
+      img: Img7,
+      link: "https://e-commerceappmern.herokuapp.com/",
+    },
+    {
+      id: 8,
+      img: Img8,
+      link: "https://hamzaumer.github.io/Music_Player_By_JS/",
+    },
+    {
+      id: 9,
+      img: Img9,
+      link: "https://github.com/HamzaUmer/NextJS-Restaurant-App",
     },
   ];
